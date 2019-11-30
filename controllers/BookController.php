51950981116b16d7f27a030b6cd8112d75bd8ef1
abigailme <abigailmeloleon@gmail.com>
2019-11-30 19:00:56 -0300

    Adding CORS to Rest Api

--- controllers/BookController.php
+++ controllers/BookController.php
@@ -4,11 +4,33 @@ namespace app\controllers;
 
 use yii\rest\ActiveController;
 use yii\data\ActiveDataProvider;
+use yii\filters\auth\HttpBasicAuth;
 
 class BookController extends ActiveController
 {
 
 	public $modelClass = 'app\models\Book';
+
+	public function behaviors()
+{
+    $behaviors = parent::behaviors();
+
+    // remove authentication filter
+    $auth = $behaviors['authenticator'];
+    unset($behaviors['authenticator']);
+
+    // add CORS filter
+    $behaviors['corsFilter'] = [
+        'class' => \yii\filters\Cors::className(),
+    ];
+
+    // re-add authentication filter
+    $behaviors['authenticator'] = $auth;
+    // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
+    $behaviors['authenticator']['except'] = ['options'];
+
+    return $behaviors;
+}
 
     public function actionSearch(){
 	    if (!empty($_GET)) {
